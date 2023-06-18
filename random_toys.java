07448ef560788b2b499e4721079e69f87bf3ac5d
Dasha <daryaishchuk@gmail.com>
2023-06-18 13:36:49 +0300

    Java

---
+++ random_toys.java
@@ -0,0 +1,39 @@
+id: 3, weight: 6, name: кубики
+id: 1, weight: 2, name: мишка
+id: 2, weight: 2, name: машинка
+
+id: 3, weight: 6, name: кубики
+id: 1, weight: 2, name: мишка
+id: 2, weight: 2, name: машинка
+
+id: 3, weight: 6, name: кубики
+id: 1, weight: 2, name: мишка
+id: 2, weight: 2, name: машинка
+
+id: 3, weight: 6, name: кубики
+id: 1, weight: 2, name: мишка
+id: 2, weight: 2, name: машинка
+
+id: 3, weight: 6, name: кубики
+id: 1, weight: 2, name: мишка
+id: 2, weight: 2, name: машинка
+
+id: 3, weight: 6, name: кубики
+id: 1, weight: 2, name: мишка
+id: 2, weight: 2, name: машинка
+
+id: 3, weight: 6, name: кубики
+id: 1, weight: 2, name: мишка
+id: 2, weight: 2, name: машинка
+
+id: 3, weight: 6, name: кубики
+id: 1, weight: 2, name: мишка
+id: 2, weight: 2, name: машинка
+
+id: 3, weight: 6, name: кубики
+id: 1, weight: 2, name: мишка
+id: 2, weight: 2, name: машинка
+
+id: 3, weight: 6, name: кубики
+id: 1, weight: 2, name: мишка
+id: 2, weight: 2, name: машинка
